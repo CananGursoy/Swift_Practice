004391f809f6e5d82ce8ca131aa224e039492af6
Hatice Gursoy <hgursoy@bloomberg.net>
2016-07-26 14:27:39 -0400

    Add To Do List app

---
+++ To Do List/To Do List/FirstViewController.swift
@@ -0,0 +1,69 @@
+//
+//  FirstViewController.swift
+//  To Do List
+//
+//  Created by Hatice Gursoy on 7/4/16.
+//  Copyright © 2016 Hatice Gursoy. All rights reserved.
+//
+
+import UIKit
+
+var toDoList = [String]()
+
+class FirstViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var toDoListTable: UITableView!
+
+   
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    return toDoList.count
+        
+    }
+    
+   
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+   
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = toDoList[indexPath.row]
+        return cell
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        toDoListTable.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+            toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            toDoList.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            toDoListTable.reloadData()
+        
+        }
+        
+        
+    
+    }
+
+   
+
+}
+
